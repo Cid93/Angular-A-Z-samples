32cc845a32a95a1796db7c3c0ae691fc79444e0b
OUTREVILLE Cédric <outreville.c@paris.sfeir.com>
2016-10-16 18:58:42 +0200

    add dire-bonjour component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { DireBonjourComponent } from './components/dire-bonjour/dire-bonjour.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DireBonjourComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/dire-bonjour/dire-bonjour.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input, Output, OnInit, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-dire-bonjour',
+  templateUrl: 'dire-bonjour.component.html',
+  styleUrls: ['dire-bonjour.component.css']
+})
+export class DireBonjourComponent implements OnInit {
+
+@Input('nom') inputPropriete: string;
+@Output('clique') outputEvenement: EventEmitter<string>;
+
+  constructor() {
+      this.outputEvenement = new EventEmitter<string>();
+  }
+
+  ngOnInit() {
+  }
+
+  clicked(nom: string) {
+      this.outputEvenement.emit(nom);
+  }
+
+}
